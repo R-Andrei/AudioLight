b82a451f18db0f75909f89a21d780d7c218661f4
R-Andrei <faragau.rares@gmail.com>
2020-03-20 18:18:46 +0200

    testing ffmpeg

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -2,20 +2,25 @@ from AudioConverter import AudioConverter
 from FrequencyConverter import FrequencyConverter
 import json
 
+from pydub import AudioSegment
+
 def main():
 
-    fconverter = FrequencyConverter()
-    aconverter = AudioConverter(fconverter)
+    # fconverter = FrequencyConverter()
+    # aconverter = AudioConverter(fconverter)
 
-    aconverter.import_directory('songs/')
+    # aconverter.import_directory('../songs/')
 
-    aconverter.export_store('output_me.json')
+    # aconverter.export_store('../output/output_me.json')
 
-    output = {}
+    # output = {}
 
-    output['data'] = list(aconverter.test_spectrum(gamma=0.9, true=False))
-    with open('spectrum.json', 'w') as spectrum_file:
-        json.dump(output, spectrum_file)
+    # output['data'] = list(aconverter.test_spectrum(gamma=0.9, true=False))
+    # with open('spectrum.json', 'w') as spectrum_file:
+    #     json.dump(output, spectrum_file)
+
+    sound = AudioSegment.from_mp3('../media/dl/Vera Blue - Hold.mp3')
+    print(sound)
 
 
 if __name__ == "__main__":
