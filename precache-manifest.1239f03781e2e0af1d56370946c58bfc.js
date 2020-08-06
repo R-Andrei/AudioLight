dd14f381e23c22c765c3b1766a2624fe613c84d9
R-Andrei <faragau.rares@gmail.com>
2020-08-07 00:02:41 +0300

    deploy (fixed public path)

---
+++ precache-manifest.1239f03781e2e0af1d56370946c58bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e66a7a05a544fe9bb7ff41bda35a43",
+    "url": "/AudioLight/index.html"
+  },
+  {
+    "revision": "953e23567ff9595b0085",
+    "url": "/AudioLight/static/css/main.22c1dfc1.chunk.css"
+  },
+  {
+    "revision": "fb284dbdaf8ab184fe06",
+    "url": "/AudioLight/static/js/2.cceeabfa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/AudioLight/static/js/2.cceeabfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953e23567ff9595b0085",
+    "url": "/AudioLight/static/js/main.277e1c7a.chunk.js"
+  },
+  {
+    "revision": "b6324bd476cc82e0e72d",
+    "url": "/AudioLight/static/js/runtime-main.baf77a6c.js"
+  }
+]);
